f4e96618e6f100d9bf65651ad0e0bc1d9d0431bd
Masahiro Takata <takata005581016@gmail.com>
2020-06-18 00:00:01 +0900

    Initial Commit

---
+++ movieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  movieApp
+//
+//  Created by 高田将弘 on 2020/06/17.
+//  Copyright © 2020 高田将弘. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
